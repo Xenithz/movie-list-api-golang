c72d10b97f0abd7cdcef4676e08477789c4283fd
Angelo Guerrero <amcgdev@gmail.com>
2019-08-19 00:16:14 +0400

    Added rest of get queries, have to refactor code because there is alot of boiler plate

--- queries/queries.go
+++ queries/queries.go
@@ -1,8 +1,6 @@
 package queries
 
 import (
-	"fmt"
-
 	mydb "github.com/xenithz/movie-list-api-golang/database"
 	types "github.com/xenithz/movie-list-api-golang/movie-types"
 
@@ -27,7 +25,158 @@ func GetAllMovies(Movies *types.Movies) error {
 	defer rows.Close()
 
 	for rows.Next() {
-		fmt.Println("adding movie")
+		movie := types.Movie{}
+		err = rows.Scan(
+			&movie.ID,
+			&movie.MovieID,
+			&movie.MovieTitle,
+			&movie.MovieGenre,
+			&movie.MovieDirector,
+		)
+		if err != nil {
+			return err
+		}
+		Movies.Movies = append(Movies.Movies, movie)
+	}
+	err = rows.Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetMovieByID(Movies *types.Movies, id int) error {
+	db := mydb.ConnectToDB()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	err := db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	rows, err := db.Query(`SELECT * FROM movies WHERE movie_id = ?`, id)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		movie := types.Movie{}
+		err = rows.Scan(
+			&movie.ID,
+			&movie.MovieID,
+			&movie.MovieTitle,
+			&movie.MovieGenre,
+			&movie.MovieDirector,
+		)
+		if err != nil {
+			return err
+		}
+		Movies.Movies = append(Movies.Movies, movie)
+	}
+	err = rows.Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetMovieByTitle(Movies *types.Movies, title string) error {
+	db := mydb.ConnectToDB()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	err := db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	rows, err := db.Query(`SELECT * FROM movies WHERE movie_title = ?`, title)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		movie := types.Movie{}
+		err = rows.Scan(
+			&movie.ID,
+			&movie.MovieID,
+			&movie.MovieTitle,
+			&movie.MovieGenre,
+			&movie.MovieDirector,
+		)
+		if err != nil {
+			return err
+		}
+		Movies.Movies = append(Movies.Movies, movie)
+	}
+	err = rows.Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetMovieByGenre(Movies *types.Movies, genre string) error {
+	db := mydb.ConnectToDB()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	err := db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	rows, err := db.Query(`SELECT * FROM movies WHERE movie_genre = ?`, genre)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		movie := types.Movie{}
+		err = rows.Scan(
+			&movie.ID,
+			&movie.MovieID,
+			&movie.MovieTitle,
+			&movie.MovieGenre,
+			&movie.MovieDirector,
+		)
+		if err != nil {
+			return err
+		}
+		Movies.Movies = append(Movies.Movies, movie)
+	}
+	err = rows.Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetMovieByDirector(Movies *types.Movies, director string) error {
+	db := mydb.ConnectToDB()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	err := db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	rows, err := db.Query(`SELECT * FROM movies WHERE movie_director = ?`, director)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
 		movie := types.Movie{}
 		err = rows.Scan(
 			&movie.ID,
